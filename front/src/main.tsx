3089174d255ad4c66d533c329427172e9b2edc25
ktk8916 <ktk8916@naver.com>
2025-02-11 01:02:48 +0900

    init

---
+++ front/src/main.tsx
@@ -0,0 +1,34 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router';
+import App from './App';
+import Layout from './layouts/dashboard';
+import DashboardPage from './pages';
+import OrdersPage from './pages/orders';
+
+
+const router = createBrowserRouter([
+  {
+    Component: App,
+    children: [
+      {
+        path: '/',
+        Component: Layout,
+        children: [
+          {
+            path: '',
+            Component: DashboardPage,
+          },
+          {
+            path: 'orders',
+            Component: OrdersPage,
+          },
+        ],
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+    <RouterProvider router={router} />
+);
